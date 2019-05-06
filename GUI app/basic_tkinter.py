7332caac463ac63c024d25c3c2b2f97f3d05777f
gurpreets <montycarlo017@gmail.com>
2019-05-06 15:08:12 +0100

    GUI Commit

---
+++ GUI app/basic_tkinter.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+root = Tk()
+theLabel = Label(root, text = 'This is my first GUI app')
+theLabel.pack() # This actually puts the text in the GUI window.
+root.mainloop() # This will keep the program running until we close the window manually.
