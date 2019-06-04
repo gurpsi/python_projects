d5edaae1643fe404fda390758bba32d7782e5ff5
gurpreets <montycarlo017@gmail.com>
2019-06-04 22:27:57 +0100

    GUI Commit: Tab 3 Images

--- GUI app/Live data with tab 3 functionality.py
+++ GUI app/Live data with tab 3 functionality.py
@@ -4,6 +4,8 @@ In this we will populate the Live data, in GUI application but on button click.
 After completion of tab 2, we will now proceed on for the tab 3 functionality:
 
 # TAB 3: Using the Pillow Library for Image manipulation and loading the images in GUI app.
+
+The images for this are free to use.
 '''
 
 import tkinter as tk
