035b995510423aa48ec0867c136d8cfb49ab605f
gurpreets <montycarlo017@gmail.com>
2019-09-07 10:23:52 +0100

    Comments

--- GUI app/Live data with tab 3 functionality.py
+++ GUI app/Live data with tab 3 functionality.py
@@ -5,7 +5,7 @@ After completion of tab 2, we will now proceed on for the tab 3 functionality:
 
 # TAB 3: Using the Pillow Library for Image manipulation and loading the images in GUI app.
 
-Images loaded.
+Loaded Images.
 '''
 
 import tkinter as tk
