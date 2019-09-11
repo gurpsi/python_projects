4be1665b22d19ca74c905628d78348a100aafe39
gurpreets <montycarlo017@gmail.com>
2019-09-11 07:50:15 +0100

    Basic stack implementation.

--- Basics/stack.py
+++ Basics/stack.py
@@ -4,10 +4,26 @@ class stack:
         self.items = []
 
     def push(self,item):
-        pass
+        """
+        :param item: The item to be added in to the stack.
+        :return: Nothing
+
+        The runtime for this is O(1), or constant time because appending
+        to the last of the list happens in constant time.
+        """
+
+        self.items.append(item)
 
     def pop(self):
-        pass
+        """
+        :return: Removes and returns the last item form the list, i.e. the top item of the stack.
+
+        The runtime is constant as we are removing the last item form the list. O(1)
+        """
+        if self.items:
+            return self.items.pop()
+
+        return None
 
     def peek(self):
         pass
