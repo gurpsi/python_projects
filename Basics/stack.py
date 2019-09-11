bd8a9ed03812e158b8644b4d17b42e3966e9d966
gurpreets <montycarlo017@gmail.com>
2019-09-11 07:30:46 +0100

    Basic stack implementation.

---
+++ Basics/stack.py
@@ -0,0 +1,20 @@
+class stack:
+
+    def __init__(self):
+        self.items = []
+
+    def push(self,item):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def size(self):
+        pass
+
+    def is_empty(self):
+        pass
+
