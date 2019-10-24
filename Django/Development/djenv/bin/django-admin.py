9252c8cd9ed1b17ad2ccfbc757c22cf8bcbc4a37
gurpreets <montycarlo017@gmail.com>
2019-10-24 09:06:30 +0100

    Django

---
+++ Django/Development/djenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gurpreets/Desktop/python_project/Django/Development/djenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Django/Development/djenv/lib/python3.6/_bootlocale.py
@@ -0,0 +1,1 @@
+/Users/gurpreets/anaconda3/lib/python3.6/_bootlocale.py
